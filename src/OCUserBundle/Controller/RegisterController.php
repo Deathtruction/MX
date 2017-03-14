a847020d1ccd541a1771b417edc8f28f47cd647f
christo169 <christopher.ol@hotmail.fr>
2017-03-14 10:24:01 +0100

    réparation de l'ancien projet
    https://github.com/Deathtruction/ProjetN2

---
+++ src/OCUserBundle/Controller/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace OCUserBundle\Controller;
+
+use OCUserBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('', array('name' => $name));
+    }
+
+
+    /**
+     * @Route("/register")
+     */
+    public function load()
+    {
+
+        $user = new User;
+        $user->setUsername('test');
+        $user->setPassword('test');
+        $user->setSalt('');
+        $user->setRoles(array('ROLE_USER'));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+
+        $em->flush();
+
+    }
+}
---
+++ src/OCUserBundle/OCUserBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OCUserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OCUserBundle extends Bundle
+{
+
+
+}
