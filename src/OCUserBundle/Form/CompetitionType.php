7d5ff050bb5d17de3c2dda37c9acf95e09bff6de
christo169 <christopher.ol@hotmail.fr>
2017-03-31 15:39:06 +0200

    modif type compétition

--- src/OCUserBundle/Form/CompetitionType.php
+++ src/OCUserBundle/Form/CompetitionType.php
@@ -3,6 +3,8 @@
 namespace OCUserBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,15 +16,40 @@ class CompetitionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date')
+            ->add('date', DateType::class, array(
+                'widget' => 'single_text',
+                'placeholder' => array(
+                    'year' => 'Year',
+                    'month' => 'Month',
+                    'day' => 'Day'
+                ),
+            ))
             ->add('lieu')
             ->add('departement')
             ->add('club')
             ->add('correspondants')
             ->add('cylindreAccepter')
-            ->add('type')
-            ->add('dateLimiteInscription')
-            ->add('inscriptionOuverte')
+            ->add('type', ChoiceType::class, array(
+                'choices' => array(
+                    'Championnat' => 'type',
+                    'Entrainement officiel' => 'type',
+                    'Rencontre inter-club' => 'type'
+                )
+            ))
+            ->add('dateLimiteInscription', DateType::class, array(
+                'widget' => 'single_text',
+                'placeholder' => array(
+                    'year' => 'Year',
+                    'month' => 'Month',
+                    'day' => 'Day'
+                )
+            ))
+            ->add('inscriptionOuverte', ChoiceType::class, array(
+                'choices' => array(
+                    'oui' => true,
+                    'non' => false
+                )
+            ))
             ->add('nbParticipants');
     }
     
